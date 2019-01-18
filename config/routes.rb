7ca2c883ae02142d83ef4be0e5294f0fb69222d2
shiota yoshiki <shiotyan317@gmail.com>
2019-01-19 01:57:49 +0900

    add ログイン後の画面の作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "top#index"
 
+  namespace :admin do
+    resources :events
+  end
 end
