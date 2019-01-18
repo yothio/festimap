074f859feb395c8bc76ba14e0dd588b67adbd09c
shiota yoshiki <shiotyan317@gmail.com>
2019-01-19 04:42:26 +0900

    fix ログアウト時のアクションをgetに変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users, path_names: { sign_in: "login", sign_out: "logout"}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "top#index"
+  # root to: "top#index"
 
   namespace :admin do
     resources :events
     resources :users
+  end
+
+  devise_scope :users do
+    get 'logout' => 'devise/sessions#destroy'
   end
 end
